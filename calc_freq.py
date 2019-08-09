b8c342146b9dce6ae5f1cc0a1524180085e8013c
LeaFZ <leafzhang714@gmail.com>
2019-08-09 10:19:38 -0500

    modified calc

--- calc_freq.py
+++ calc_freq.py
@@ -5,6 +5,7 @@ line = inputfile.readline()
 
 addr_dict = {}
 delta_dict = {}
+pc_dict = {}
 
 while line:
     line_split = line.split()
@@ -15,6 +16,11 @@ while line:
         addr_dict[addr] += 1
     else:
         addr_dict[addr] = 1
+
+    if pc_dict.__contains__(pc):
+        pc_dict[pc] += 1
+    else:
+        pc_dict[pc] = 1
 
     if delta_dict.__contains__(delta):
         delta_dict[delta] += 1
@@ -28,11 +34,16 @@ inputfile.close()
 
 addr_sorted = sorted(addr_dict.items(), key=lambda item:item[1], reverse=True)
 delta_sorted = sorted(delta_dict.items(), key=lambda item:item[1], reverse=True)
+pc_sorted = sorted(pc_dict.items(), key=lambda item:item[1], reverse=True)
 
 outputfile = open('calc.out', 'w')
 outputfile.write('addr: \n')
 for i in addr_sorted:
     outputfile.writelines(str(i) + '\n')
+
+outputfile.write('pc: \n')
+for i in pc_sorted:
+    outputfile.write(str(i) + '\n')
 
 outputfile.write('delta: \n')
 for i in delta_sorted:
